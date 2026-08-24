9b9ec9f2a3ac0e6f6207dd939f8d96f75d9b82c6
박수훈 <86775077+PrakSuHun@users.noreply.github.com>
2026-08-25 07:41:57 +0900

    브랜드 중립 관리자 주소 분리

--- brand-config.js
+++ brand-config.js
@@ -1,5 +1,6 @@
 // 저장소별 관리자 화면에 표시할 브랜드명입니다.
 window.BRAND_SITE = Object.freeze({
   name: "에코 아카이브",
-  adminUrl: "",
+  adminUrl: "https://eco-archive-admin.progen-web.workers.dev/admin",
+  siteUrl: "https://eco-archive-gpu.progen-web.workers.dev",
 });
